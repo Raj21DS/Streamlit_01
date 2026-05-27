d1414ff9b2addcaec1436b3062122bbafe435e1c
Raj Kumar <Analyst.k.raj@gmail.com>
2026-05-27 22:00:07 +0530

    Update Demo01.py

--- Demo01.py
+++ Demo01.py
@@ -1,6 +1,8 @@
 import streamlit as st
+
+st.set_page_config(layout = "wide")
 st.write("Hello World")
 x = st.text_input("Favourite Movie?")
 st.write(f"your favourite movie is: {x}")
 st.write("## This is a H2 Title")
-# st.set_page_config(layout = "wide")
+
