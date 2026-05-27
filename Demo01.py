da14a3ec08c6ed9796bdb4ef393f1c43b688808f
Raj Kumar <Analyst.k.raj@gmail.com>
2026-05-27 22:19:56 +0530

    Update Demo01.py

--- Demo01.py
+++ Demo01.py
@@ -8,3 +8,18 @@ st.write(f"your favourite movie is: {x}")
 st.write("## This is a H2 Title")
 stream_url ="https://www.youtube.com/watch?v=D1eL1EnxXXQ"
 st.video(stream_url)
+
+response = requests.get(stream_url, params=params)
+data = response.json()
+
+if "items" in data and len(data["items"]) > 0:
+  stats = data["items"][0]["statistics"]
+  return {
+            "views": stats.get("viewCount", 0),
+            "likes": stats.get("likeCount", 0),
+            "comments": stats.get("commentCount", 0)
+        }
+  return None
+st.write(f"👁 Views: {stats['views']}")
+st.write(f"👍 Likes: {stats['likes']}")
+st.write(f"💬 Comments: {stats['comments']}")
