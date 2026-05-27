451db6e074283ab92a220ae734ba7659caf2e7ce
Raj Kumar <Analyst.k.raj@gmail.com>
2026-05-27 22:01:50 +0530

    Update Demo01.py

--- Demo01.py
+++ Demo01.py
@@ -5,4 +5,5 @@ st.write("Hello World")
 x = st.text_input("Favourite Movie?")
 st.write(f"your favourite movie is: {x}")
 st.write("## This is a H2 Title")
-
+stream_url ="https://www.youtube.com/watch?v=D1eL1EnxXXQ"
+st.video(stream_url)
